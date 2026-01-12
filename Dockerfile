325d5b3f5676f516d73bb69f1897b4c3a1d32e8b
root <root@SlavikLegion.>
2026-01-12 16:25:27 +0100

    Validation Challenge 3

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:alpine3.23
 WORKDIR /app
-COPY . /app
-RUN pip install flask
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY app.py .
 EXPOSE 5000
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 import socket
 from datetime import datetime
+import os
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient(os.getenv("MONGO_URI"))
+db = client.net4255
+logs_collection = db.logs
 
 @app.route("/")
 def index():
     name = "Slavik"
-    project = "net4255-kubernetes-Slavik-c1"
-    version = "V1"
+    project = "net4255-kubernetes-Slavik-c3"
+    version = "V2"
     hostname = socket.gethostname()
     current_date = datetime.now()
+    client_ip = request.remote_addr
+
+    logs_collection.insert_one({
+        "ip": client_ip, 
+        "date": current_date
+    })
+
+    last_logs = list(logs_collection.find().sort("date", -1).limit(10))
+    
+    logs_html = ""
+    for log in last_logs:
+        logs_html += f"<li>IP: {log['date']} | Date : {log['ip']}</li>"
 
     return f"""
     <p><b>Name:</b> {name}</p>
@@ -18,6 +36,11 @@ def index():
     <p><b>Version:</b> {version}</p>
     <p><b>Server Hostname:</b> {hostname}</p>
     <p><b>Current Date:</b> {current_date}</p>
+
+    <h3>Last 10 visits:</h3>
+    <ul>
+        {logs_html}
+    </ul>
     """
 
 if __name__ == "__main__":
